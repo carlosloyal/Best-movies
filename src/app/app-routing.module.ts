76fe62afe210e0062d87acf24ff5303ba5e8335c
carlosloyal <97996337+carlosloyal@users.noreply.github.com>
2022-04-02 03:33:59 -0500

    routing feature usage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BiographyComponent } from './movies/biography/biography.component';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "biography",
+    component: BiographyComponent
+  },
+
+  {
+    path: "movies-list",
+    component: MoviesListComponent
+  
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { BiographyComponent } from './movies/biography/biography.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MoviesListComponent,
+    BiographyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatSidenavModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
